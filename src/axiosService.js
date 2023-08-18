a6fe17e0955ed19e39388fd0b8dc49f685fbfbbb
priyansh19csu225 <71059426+priyansh19csu225@users.noreply.github.com>
2023-08-19 01:40:31 +0530

    one more change

--- src/axiosService.js
+++ src/axiosService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 const CURRENTUSERDETAILS = localStorage.getItem('persist:root');
 const CURRENTUSEROBJECT = JSON.parse(CURRENTUSERDETAILS);
-const CURRENTUSERTOKEN = JSON.parse(CURRENTUSEROBJECT.user).currentUser.token;
+const CURRENTUSERTOKEN = JSON.parse(CURRENTUSEROBJECT?.user)?.currentUser?.token;
 export const publicRequest = axios.create({
     baseURL:BASE_URL
 });
